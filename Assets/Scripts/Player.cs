43d2e158b8239be09848b8d1673c78cee05c7489
Aditya Ramesh <rameshaditya2001@gmail.com>
2020-07-09 00:13:43 +0530

    Created the Spawn Manager
    Enemies spawn at the rate of 5 seconds.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,11 +14,17 @@ public class Player : MonoBehaviour
     private float _canFire = -1f;
     [SerializeField]
     private int _lives = 3;
+    private SpawnManager _spawnManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0,0,0); 
+        transform.position = new Vector3(0,0,0);
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
+        if(_spawnManager == null)
+        {
+            Debug.LogError("The Spawn Manager is null.");
+        }
     }
 
     
@@ -76,6 +82,7 @@ public class Player : MonoBehaviour
 
         if(_lives == 0)
         {
+            _spawnManager.OnPlayerDeath();
             Destroy(this.gameObject);
         }
     }
